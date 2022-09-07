cba976e73b9bf9b9345e8b88a1a66af12ad41116
KEY_Cheng <78220238+key-cc@users.noreply.github.com>
2022-09-07 07:22:02 +0200

    Update download_ucf101.sh

--- data/download_ucf101.sh
+++ data/download_ucf101.sh
@@ -2,5 +2,5 @@
 
 # Downloads the UCF-101 dataset
 
-wget http://crcv.ucf.edu/data/UCF101/UCF101.rar
-wget https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip
+wget http://crcv.ucf.edu/data/UCF101/UCF101.rar --no-check-certificate
+wget https://www.crcv.ucf.edu/data/UCF101/UCF101TrainTestSplits-RecognitionTask.zip --no-check-certificate
